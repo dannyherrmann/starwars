f529c6d1f7c59d0cd58f1d653823073ed1d7f432
Danny Herrmann <dannyherrmann91@gmail.com>
2022-09-14 21:07:51 -0500

    uploading

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,15 @@
-import {fetchLuke} from "./starWarsData.js"
+import {fetchLuke, fetchFilms, fetchStarshipsByPage, fetchPlanets} from "./starWarsData.js"
+
 
 const displayLuke = async () => {
   const data = await fetchLuke()
   renderLukeToDOM(data)
 }
 
+let html = ''
+
 const renderLukeToDOM = (data) => {
-  let html = `
+  html += `
     <article>
       <section class="card">
         <p>Name: ${data.name}</p>
@@ -17,5 +20,89 @@ const renderLukeToDOM = (data) => {
   document.getElementById('app').innerHTML = html
 }
 
-displayLuke()
+const displayFilms = async () => {
+  const filmData = await fetchFilms()
+  console.log('Film Data: ',filmData)
+  const filmResults = filmData.results
+  console.log('Film Results: ',filmResults)
+  renderFilmsToDOM(filmResults)
+}
+
+const renderFilmsToDOM = (films) => {
+
+  for (const film of films) {
+    html += `
+    <article>
+      <section class="card">
+        <p><b>Title:</b> ${film.title}</p>
+        <p><b>Director:</b> ${film.director}</p>
+        <p><b>Release Date:</b> ${film.release_date}
+        <p><b>Opening Crawl:</b> ${film.opening_crawl}
+      </section>
+    </article>
+  `
+  }
+
+  document.getElementById('app').innerHTML = html
+
+}
+
+const displayStarships = async (num) => {
+  const starshipData = await fetchStarshipsByPage(num)
+  console.log(`Starship Data: `, starshipData)
+  renderStarshipsToDOM(starshipData)
+}
+
+const renderStarshipsToDOM = (starships) => {
+  console.log(`starship_count: `, starships.results.length)
+  html += `
+    <article>
+      <section class="card">
+        <p>There are ${starships.results.length} starships on page #4</p>
+      </section>
+    </article>
+  `
+  document.getElementById('app').innerHTML = html
+}
+
+const displayPlanets = async () => {
+  const planetData1 = await fetchPlanets(1)
+  const planetData2 = await fetchPlanets(2)
+  const planetData3 = await fetchPlanets(3)
+  const planetData4 = await fetchPlanets(4)
+  const planetData5 = await fetchPlanets(5)
+  const planetData6 = await fetchPlanets(6)
+  let allPlanets = [...planetData1, ...planetData2, ...planetData3, ...planetData4, ...planetData5, ...planetData6]
+  console.log('allPlanets: ',allPlanets)
+  allPlanets.sort((a, b) => a.diameter - b.diameter)
+  renderPlanetsToDOM(allPlanets)
+}
+
+
+const displayPlanets2 = async () => {
+  const planetData = await fetchPlanets()
+}
+
+const renderPlanetsToDOM = (planets) => {
+
+  for (const planet of planets) {
+    html += `
+      <article>
+        <section class="card">
+        <h3>Planet: ${planet.name}
+          <ul>
+            <li>Diameter: ${planet.diameter}</li>
+          </ul>
+        </section>
+      </article>
+    `
+  }
+  document.getElementById('app').innerHTML = html
+
+}
+
+await displayLuke()
+await displayFilms()
+await displayStarships(4)
+await displayPlanets()
 
--- scripts/starWarsData.js
+++ scripts/starWarsData.js
@@ -1,10 +1,32 @@
-const API = "https://swapi.dev/api"
+const API = "https://swapi.dev/api/"
 
-// Get Luke Skywalker
+//Get Luke Skywalker
 export const fetchLuke = async () => {
   const dataFetch = await fetch(`${API}/people/1`)
-  const jsonData = await dataFetch.json()
-  return jsonData
+  const lukeData = await dataFetch.json()
+  return lukeData
+}
+
+// Get Films
+export const fetchFilms = async () => {
+  const dataFetch = await fetch(`${API}/films/`)
+  const filmData = await dataFetch.json()
+  return filmData
 };
+
+// Get Starships by pageNum
+export const fetchStarshipsByPage = async (pageNum) => {
+  const dataFetch = await fetch(`${API}starships/?page=${pageNum}`)
+  const starshipData = await dataFetch.json()
+  return starshipData
+}
+
+//Get Plants
+export const fetchPlanets = async (pageNum) => {
+  const dataFetch = await fetch(`${API}/planets/?page=${pageNum}`)
+  const planetData = await dataFetch.json()
+  return planetData.results
+}
+
 
 
